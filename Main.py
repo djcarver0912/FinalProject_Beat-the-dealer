3a61f2ebc5a7940d69abf9ef00c748256732d501
djcarver0912 <djcarver@students.everettcc.edu>
2025-11-25 03:03:47 +0000

    Code roughly done need alg + rdme

--- Main.py
+++ Main.py
@@ -14,8 +14,13 @@ while True:
     random.shuffle(deck)
 
 #Deal cards
-    player = [deck.pop(), deck.pop()]
-    dealer = [deck.pop(), deck.pop()]
+    def draw_card():
+        card = random.choice(deck)
+        deck.remove(card)
+        return card
+    
+    player = [draw_card(), draw_card()]
+    dealer = [draw_card(), draw_card()]
 
 #First, the player's turn
     while True:
@@ -28,7 +33,7 @@ while True:
         
         choice = input("hit or stand? ").lower()
         if choice == "hit":
-            player.append(deck.pop())
+            player.append(draw_card())
         else:
             break
 
@@ -37,12 +42,31 @@ while True:
 
 #Dealer Turn
         print("Dealer's Hand:", dealer, "Total =", calculate_total(dealer))   
-        while calculate_total(dealer < 17):
-            dealer.append(deck.pop)
+        while calculate_total(dealer) < 17:
+            dealer.append(draw_card())
             print("Dealer Hits:", dealer, "Total=", calculate_total(dealer))
 
 
 #Show hand and determine winner
-        
+        player_total = calculate_total(player)
+        dealer_total = calculate_total(dealer)
+
+        print("\n ****    Final Results    ****")
+        print("Player:", player_total)
+        print("Dealer:", dealer_total)
+
+
+        if dealer_total > 21:
+            print("Dealer Busts, you win!")
+        elif player_total > dealer_total:
+            print("You win!")
+        elif dealer_total > player_total:
+            print("Dealer Wins")
+        else:
+            print("It is a tie")
 
 #Potential loop or exit for the player
+    play_again = input("\n Would you like to play again? (y/n): ").lower()
+    if play_again != "y":
+        print("Thank you for playing!")
+        break
