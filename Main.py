7c245744fa18f7336da879d1ad18df39afbbae00
djcarver0912 <djcarver@students.everettcc.edu>
2025-11-26 18:29:42 +0000

    Alg + rdme complete. Just chk for er

--- Main.py
+++ Main.py
@@ -1,72 +1,72 @@
-#Bring in python's random library
 import random #This brings in the random library so we may generate a random value. 
 
-def calculate_total(hand):
+#A function for situations where you could convert an ace to a 1 to prevent a bust. 
+def calculate_total(hand):  
     total = sum(hand)
-    while total > 21 and 11 in hand:
+    while total > 21 and 11 in hand: #If the player has more than 21 in their hands, with an ace (11), convert to (1)
         hand[hand.index(11)] = 1
-        total = sum(hand)
+        total = sum(hand) #Recalculate the total now that the conversion is complete.
     return total
 
 #Shuffle deck
 while True:
-    deck = [2,3,4,5,6,7,8,9,10,10,10,10,11] * 4
-    random.shuffle(deck)
+    deck = [2,3,4,5,6,7,8,9,10,10,10,10,11] * 4 #Values 2-11. 10 also being face cards. Times 4 for suits
+    random.shuffle(deck) #Jumble up the deck in a complete random order. 
 
 #Deal cards
     def draw_card():
-        card = random.choice(deck)
-        deck.remove(card)
+        card = random.choice(deck) #A random card will be drawn from the deck when this function is called
+        deck.remove(card) #Make sure to remove that card from the deck so it cannot be drawn again. 
         return card
     
-    player = [draw_card(), draw_card()]
-    dealer = [draw_card(), draw_card()]
+    player = [draw_card(), draw_card()] #Swtiched from deck.pop() to draw_card
+    dealer = [draw_card(), draw_card()] #Player and dealer each given two random cards
 
 #First, the player's turn
     while True:
-        print("Your hand:", player, "Total =", calculate_total(player))
+        print("Your hand:", player, "Total =", calculate_total(player)) #Make sure the player knows whats in their hand
         print("Dealer Shows:", dealer[0])
 
-        if calculate_total(player) > 21:
+        if calculate_total(player) > 21: #If you have a value over 21, you lose the game.
             print("You bust! Dealer Wins")
-            break
+            break 
         
-        choice = input("hit or stand? ").lower()
+        choice = input("hit or stand? ").lower() #If you have 21 or less, you can choose to hit or stand
         if choice == "hit":
-            player.append(draw_card())
+            player.append(draw_card()) #If you choose to hit, another random card will be placed in your hand
         else:
-            break
+            break 
 
 #If you bust 
-    if calculate_total(player) <= 21:
+    if calculate_total(player) <= 21: #After the player takes their turn, if you have a value less than/equal to 21, the game will continue. 
 
 #Dealer Turn
-        print("Dealer's Hand:", dealer, "Total =", calculate_total(dealer))   
-        while calculate_total(dealer) < 17:
+        print("Dealer's Hand:", dealer, "Total =", calculate_total(dealer))  #Show what the dealer has.
+        while calculate_total(dealer) < 17:   #The dealer will hit anytime he has a value less than 17
             dealer.append(draw_card())
-            print("Dealer Hits:", dealer, "Total=", calculate_total(dealer))
+            print("Dealer Hits:", dealer, "Total=", calculate_total(dealer)) #Display the new total to the player.
 
 
 #Show hand and determine winner
-        player_total = calculate_total(player)
+        player_total = calculate_total(player) #Calculate the player's hand here and dealer's hand in the next line
         dealer_total = calculate_total(dealer)
 
-        print("\n ****    Final Results    ****")
+        print("\n ****    Final Results    ****") #Format the display to present results.
         print("Player:", player_total)
         print("Dealer:", dealer_total)
 
 
         if dealer_total > 21:
-            print("Dealer Busts, you win!")
-        elif player_total > dealer_total:
+            print("Dealer Busts, you win!")   #If the dealer has more than 21 you win
+        elif player_total > dealer_total:   #Of course if you have more than the dealer, you win
             print("You win!")
-        elif dealer_total > player_total:
+        elif dealer_total > player_total:  #Conversely, if the dealer has more than you, dealer wins.
             print("Dealer Wins")
         else:
-            print("It is a tie")
+            print("It is a tie")    #In this project, a tie is a straight tie. The house does not win on a tie. 
 
 #Potential loop or exit for the player
-    play_again = input("\n Would you like to play again? (y/n): ").lower()
+    play_again = input("\n Would you like to play again? (y/n): ").lower() #Ask the user if they would like another round. 
     if play_again != "y":
-        print("Thank you for playing!")
+        print("Thank you for playing!") #If they don't want another round, thank them for playing and end. 
         break
--- README.md
+++ README.md
@@ -2,5 +2,4 @@ Author: Dyllan Carver
 
 Class: STEM 103
 
-Bio: # FinalProject_Beat-the-dealer
-Final Project in STEM 103. This will be a game of blackjack or 21. It will be called "Beat the Dealer"
+Bio: This program will be a short and sweet game of 21 or "BlackJack." In this program you will face the dealer in a 1 v 1 match. There is no betting on the line, just a simple goal of beating the dealer. The program will ask you whether you want to hit or stand each turn, and it will be up to your luck and skill to beat the opponent across from you. You can play as many rounds as you like. After each one, the program will ask if you want to continue. Thanks for playing. 
